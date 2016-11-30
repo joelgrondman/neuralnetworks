b322f61301ecb8ef565e6e7fe36591de7510e7e9
Joël Grondman <Jol Grondman>
2016-11-30 19:28:44 +0100

    staircase pattern! had to replace P by p, added theoretical staircase plot

--- perceptron.m
+++ perceptron.m
@@ -3,21 +3,28 @@ function [ w ] = perceptron(xi, S, nmax)
     N = size(xi,1);
     P = size(xi,2);
     w = zeros(N,1);
-
-    Ev = zeros(P,1);
+    
+    cumulative_nonupdate = 0;
     
     for t=1:nmax
         
         for p = 1:P
-
-            Ev(p) = dot(w, xi(:,p)) * S(p);
-
-            if sum(sign(Ev)) == P
-               return
+            
+            % find Ev value
+            Ev = dot(w, xi(:,p)) * S(p);
+            
+            if Ev <= 0
+                % update weight and reset cumulative_update
+                w = w + (xi(:,p) * S(p))/ N;
+                cumulative_nonupdate = 0;
+            else
+                % no update => increase nonupdate
+                cumulative_nonupdate = cumulative_nonupdate + 1;
             end
             
-            if Ev(p) <= 0
-                w = w + (xi(:,p) * S(p))/ N;
+            % P times no update, nothing will change => return
+            if cumulative_nonupdate == P
+               return 
             end
             
         end
--- test_rosenblatt.m
+++ test_rosenblatt.m
@@ -1,5 +1,7 @@
-N = 20;
-a = 2;
+clear all
+
+N = 5;
+a = 4;
 nmax = 100;
 
 P = a * N;
@@ -11,17 +13,38 @@ for p = 1:P
     
     for n = 1:nd
         %generate a set ID
-        xi = randn(N,P);
-        S = randi([0 1],P,1)*2 - 1;
+        xi = randn(N,p);
+        S = randi([0 1],p,1)*2 - 1;
         w = perceptron (xi, S, nmax);
-        output = w'* xi;
-        isld = isequal(S, sign(output)');
-        Qls = Qls + (isld == 0);
+        Qls = Qls + isequal(S, sign(w'* xi)');
     end
-    QV(p) = Qls/nd;
 
+    QV(p) = Qls;
+    
 end
+QV = QV/nd;
 
 figure(1)
 plot((1:P)/N, QV)
+
+% add theoretical value plot for comparison
+C = ones(min(N,P) + 1,1);
+if P > N
+    Vkm = zeros(P-N,1);
+    for pc = N+1:P
+        for i = 0:N-1
+           Vkm(pc - N) = Vkm(pc - N) + nchoosek(pc - 1,i);
+        end
+        Vkm(pc - N) = Vkm(pc - N)/(2^(pc - 1));
+    end
+
+  C = [C ; Vkm];  
+end
+
+X = [0:1/N:P/N]; 
+
+hold on
+stairs(X,C)
+hold off
 xlabel('P/N')
+ylabel('probability for lin. sep.')
