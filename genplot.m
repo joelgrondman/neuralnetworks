0e4567927821d52e167b5d65ea3d9de6d67a9d3f
joelgrondman <joel@sfgr.nl>
2016-12-07 14:51:52 +0100

    Add files via upload

--- genplot.m
+++ genplot.m
@@ -1,22 +1,16 @@
 function QV = gensample(N, P, nd , nmax)
 
-QV = nan(P,1);
-
-for p = 1:P
     Qls = 0;
     
     for n = 1:nd
         %generate a set ID
-        xi = randn(N,p);
-        S = randi([0 1],p,1)*2 - 1;
+        xi = randn(N,P);
+        S = randi([0 1],P,1)*2 - 1;
         w = perceptron (xi, S, nmax);
         Qls = Qls + isequal(S, sign(w'* xi)');
     end
 
-    QV(p) = Qls;
-    
-end
-QV = QV/nd;
+    QV = Qls/nd;
 
 end
 
--- rosenblatt.m
+++ rosenblatt.m
@@ -1,34 +1,35 @@
 clear all
 
 N = 5;
-a = 4;
+a = 0.75:0.25:3;
 nmax = 100;
 nd = 50;
 P = N * a;
 
 
-figure(1)
+QV = nan(size(a));
 
-if 1 == 0
-    for a = 0.75:0.25:3
-        P = N * a;
-        QV = genplot(N, P, nd , nmax);
-        hold on
-        plot((1:P)/N, QV)
-
-
-    end
+for i = 1:length(a)
+    P = floor(N * a(i));
+    QV(i) = genplot(N, P, nd , nmax);
 end
-hold off
+
+figure(1)
+plot(a, QV)
+xlabel('P/N')
+ylabel('probability for lin. sep.')
+
+
+
 
 figure(2)
 
-for N = 5:15:95
+for N = 1:10
     P = floor(N * a);
     % add theoretical value plot for comparison
     [C,X] = theoretical(N,4);
     stairs(X,C)
-    C
+    
     hold on
 end
 hold off
